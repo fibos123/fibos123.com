fe16556ea1903b1c96edd294c3f3af3f7dbdb2ca
Mingfun Wong <mingfun.wong.chn@gmail.com>
2022-08-25 10:13:02 +0000

    refine pointer class code

--- models/Pointer.ts
+++ models/Pointer.ts
@@ -19,33 +19,41 @@ class Pointer {
   async run(producerJson: IEosIoChainGetProducerJson[]) {
     this.points = [];
     this.points = producerJson.map((item) => {
+      const owner = item.owner;
+      let api_endpoint = "";
+      let ssl_endpoint = "";
+      let p2p_endpoint = "";
+      let json = [];
+      let status = EndPointStatus.waiting;
+
+      try {
+        json = JSON.parse(item.json);
+      } catch (error) {}
+
+      const nodes = json.nodes || [];
+      const full = nodes.find((x: { ssl_endpoint: any }) => x.ssl_endpoint);
+      const seed = nodes.find((x: { p2p_endpoint: any }) => x.p2p_endpoint);
+
+      if (full && full.ssl_endpoint.substring(0, 8) === "https://") {
+        api_endpoint = full.api_endpoint ? full.api_endpoint : "";
+        ssl_endpoint = full.ssl_endpoint;
+      } else {
+        status = EndPointStatus.notSet;
+      }
+
+      if (seed) {
+        p2p_endpoint = seed.p2p_endpoint;
+      }
+
       const bp: IPoints = {
-        owner: item.owner,
-        api_endpoint: "",
-        ssl_endpoint: "",
-        p2p_endpoint: "",
+        owner,
+        api_endpoint,
+        ssl_endpoint,
+        p2p_endpoint,
         number: 0,
         version: "",
         status: EndPointStatus.waiting,
       };
-      let json = [];
-      try {
-        json = JSON.parse(item.json);
-      } catch (error) {}
-      const nodes = json.nodes || [];
-      const full = nodes.find((item2: { ssl_endpoint: any }) => item2.ssl_endpoint);
-      const seed = nodes.find((item2: { p2p_endpoint: any }) => item2.p2p_endpoint);
-
-      if (full && full.ssl_endpoint.substring(0, 8) === "https://") {
-        bp.api_endpoint = full.api_endpoint ? full.api_endpoint : "";
-        bp.ssl_endpoint = full.ssl_endpoint;
-      } else {
-        bp.status = EndPointStatus.notSet;
-      }
-
-      if (seed) {
-        bp.p2p_endpoint = seed.p2p_endpoint;
-      }
 
       return bp;
     });
