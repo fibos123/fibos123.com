c056e947958cc5e7d020be61c34e1aae71b5505c
Mingfun Wong <mingfun.wong.chn@gmail.com>
2022-08-25 01:04:48 +0000

    refine date format and without dayjs

--- utils/index.ts
+++ utils/index.ts
@@ -1,15 +1,23 @@
-import dayjs from "dayjs";
-
 const formatNumber = (num: number): string => {
-  return num.toLocaleString()
-}
+  return num.toLocaleString();
+};
 
 const formatPercent = (num: number): string => {
-  return num.toFixed(2)
-}
+  return num.toFixed(2);
+};
 
-const formatDate = (date: string | number): string => {
-  return dayjs(date).format("YYYY-MM-DD HH:mm:ss")
-}
+const formatDate = (value: string | number): string => {
+  let date;
+  if ("string" === typeof value) {
+    const isZChar = value.toString().slice(-1) === "Z";
+    if (!isZChar) {
+      value = value + "Z";
+    }
+  }
+  date = new Date(value);
 
-export default { formatNumber, formatPercent, formatDate }
+  const dateString = new Intl.DateTimeFormat("zh-CN", { year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit", second: "2-digit" }).format(date);
+  return dateString;
+};
+
+export default { formatNumber, formatPercent, formatDate };
