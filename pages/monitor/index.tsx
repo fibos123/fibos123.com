b9933c3323b6c0a3e72714291035b386fd40cd30
Akira Tachibana <mingfun.wong.chn@gmail.com>
2025-10-17 01:50:36 +0000

    Revert "useAutoAnimate"
    This reverts commit 3ea3facc9506c37723c5d36aedb64e2a4cbe40ff.

--- pages/monitor/index.tsx
+++ pages/monitor/index.tsx
@@ -3,36 +3,31 @@ import Link from "next/link";
 import { Layout } from "../../components";
 import { useMonitor } from "../../hooks";
 import { formatDate, formatNumber } from "../../utils";
-import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const Page: NextPage = () => {
   const { monitors, producerStatus } = useMonitor(true);
-  const [parent] = useAutoAnimate((el, action, oldCoords, newCoords) => {
-    let keyframes;
-    keyframes = [{ opacity: 0 }, { opacity: 1 }];
-    return new KeyframeEffect(el, keyframes, { duration: 100 });
-  });
-  const [parent2] = useAutoAnimate((el, action, oldCoords, newCoords) => {
-    let keyframes;
-    keyframes = [{ opacity: 0 }, { opacity: 1 }];
-    return new KeyframeEffect(el, keyframes, { duration: 100 });
-  });
 
   return (
     <Layout title="节点监控 | FIBOS 导航">
       <main className="px-6">
         <div className="pb-4">
-          <Link href="/monitor" className="cursor-default rounded bg-indigo-500 py-2 px-4 text-white transition duration-150 ease-in-out">
+          <Link
+            href="/monitor"
+            className="cursor-default rounded bg-indigo-500 py-2 px-4 text-white transition duration-150 ease-in-out"
+          >
             出块节点在线状态
           </Link>
-          <Link href="/monitor/pointer" className="ml-4 rounded bg-white py-2 px-4 text-indigo-500 transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white">
+          <Link
+            href="/monitor/pointer"
+            className="ml-4 rounded bg-white py-2 px-4 text-indigo-500 transition duration-150 ease-in-out hover:bg-indigo-500 hover:text-white"
+          >
             接入点状态监测
           </Link>
         </div>
 
         <div className="pb-4 text-2xl">出块节点在线状态</div>
         <div>
-          <div className="flex flex-wrap gap-4 text-center" ref={parent}>
+          <div className="flex flex-wrap gap-4 text-center">
             {monitors &&
               monitors.map((item) => (
                 <div key={item.name} className="flex-1 rounded bg-white p-4">
@@ -54,11 +49,16 @@ const Page: NextPage = () => {
                   <th>最终生产时间</th>
                 </tr>
               </thead>
-              <tbody ref={parent2}>
+              <tbody>
                 {producerStatus &&
                   producerStatus.rows2.map((item, index) => (
-                    <tr key={item.bpname} className={item.bpname === producerStatus.head_block_producer ? "font-bold" : ""}>
-                      <td className={item.bpname === producerStatus.head_block_producer ? "text-slate-800" : ""}>{index + 1}</td>
+                    <tr
+                      key={item.bpname}
+                      className={item.bpname === producerStatus.head_block_producer ? "font-bold" : ""}
+                    >
+                      <td className={item.bpname === producerStatus.head_block_producer ? "text-slate-800" : ""}>
+                        {index + 1}
+                      </td>
                       <td className="whitespace-no-wrap px-4 py-2">
                         <div className="flex items-center">
                           <div className="ml-2">
@@ -69,13 +69,25 @@ const Page: NextPage = () => {
                       <td>
                         {
                           {
-                            online: <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold text-green-800">在线</span>,
-                            offline: <span className="inline-flex rounded-full bg-red-100 px-2 text-xs font-semibold text-red-800">离线</span>,
-                          }[producerStatus.head_block_num - item.number <= 242 + 10 ? "online" : "offline"]
+                            online: (
+                              <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold text-green-800">
+                                在线
+                              </span>
+                            ),
+                            offline: (
+                              <span className="inline-flex rounded-full bg-red-100 px-2 text-xs font-semibold text-red-800">
+                                离线
+                              </span>
+                            ),
+                          }[producerStatus.head_block_num - item.number <= (242+10) ? "online" : "offline"]
                         }
                       </td>
-                      <td className={item.bpname === producerStatus.head_block_producer ? "text-slate-800" : ""}>{formatNumber(item.number)}</td>
-                      <td className={item.bpname === producerStatus.head_block_producer ? "text-slate-800" : ""}>{formatDate(item.date)}</td>
+                      <td className={item.bpname === producerStatus.head_block_producer ? "text-slate-800" : ""}>
+                        {formatNumber(item.number)}
+                      </td>
+                      <td className={item.bpname === producerStatus.head_block_producer ? "text-slate-800" : ""}>
+                        {formatDate(item.date)}
+                      </td>
                     </tr>
                   ))}
               </tbody>
