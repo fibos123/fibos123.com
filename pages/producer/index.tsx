7c789c6dc10040aa4de335c3b0ba06fb884b66b8
Akira Tachibana <mingfun.wong.chn@gmail.com>
2025-10-17 01:50:45 +0000

    Revert "useAutoAnimate"
    This reverts commit 3ea3facc9506c37723c5d36aedb64e2a4cbe40ff.

--- pages/producer/index.tsx
+++ pages/producer/index.tsx
@@ -3,16 +3,10 @@ import Link from "next/link";
 import { Layout } from "../../components";
 import { useHeadBlockInfo, useProducers } from "../../hooks";
 import { formatPercent, formatNumber } from "../../utils";
-import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const Page: NextPage = () => {
   const { info } = useHeadBlockInfo(true);
   const { bpList } = useProducers();
-  const [parent] = useAutoAnimate((el, action, oldCoords, newCoords) => {
-    let keyframes;
-    keyframes = [{ opacity: 0 }, { opacity: 1 }];
-    return new KeyframeEffect(el, keyframes, { duration: 100 });
-  });
 
   return (
     <Layout title="节点列表 | FIBOS 导航">
@@ -20,7 +14,7 @@ const Page: NextPage = () => {
         <div className="px-6">
           <div className="pb-4 text-2xl">节点列表</div>
 
-          <div className="flex flex-wrap gap-4 text-center" ref={parent}>
+          <div className="flex flex-wrap gap-4 text-center">
             {info &&
               info.map((item) => (
                 <div key={item.name} className="flex-1 rounded bg-white p-4">
@@ -50,7 +44,14 @@ const Page: NextPage = () => {
                 {bpList.map((item, index) => (
                   <tr key={index}>
                     <td className="text-center text-slate-400">{index + 1}</td>
-                    <td className="px-2 py-2 text-center">{item.logo && <span className="mx-auto block h-12 w-12 bg-cover" style={{ backgroundImage: "url(" + item.logo + ")" }}></span>}</td>
+                    <td className="px-2 py-2 text-center">
+                      {item.logo && (
+                        <span
+                          className="mx-auto block h-12 w-12 bg-cover"
+                          style={{ backgroundImage: "url(" + item.logo + ")" }}
+                        ></span>
+                      )}
+                    </td>
                     <td>
                       <div>{item.candidate_name}</div>
                       <div className="text-slate-400">{item.owner}</div>
@@ -58,8 +59,16 @@ const Page: NextPage = () => {
                     <td>
                       {
                         {
-                          active: <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold text-green-800">当选节点</span>,
-                          waiting: <span className="inline-flex rounded-full bg-slate-100 px-2 text-xs font-semibold text-slate-800">待机节点</span>,
+                          active: (
+                            <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold text-green-800">
+                              当选节点
+                            </span>
+                          ),
+                          waiting: (
+                            <span className="inline-flex rounded-full bg-slate-100 px-2 text-xs font-semibold text-slate-800">
+                              待机节点
+                            </span>
+                          ),
                         }[index + 1 <= 21 ? "active" : "waiting"]
                       }
                     </td>
@@ -68,14 +77,24 @@ const Page: NextPage = () => {
                       <div className="text-sm text-slate-400">{formatNumber(item.staked)} FO</div>
                     </td>
                     <td className="text-slate-400">{formatNumber(item.claimRewardsTotal)} FO</td>
-                    <td className={"" + (item.claimRewardsUnreceived ? "font-bold text-green-500" : "text-slate-400")}>{formatNumber(item.claimRewardsUnreceived)} FO</td>
+                    <td className={"" + (item.claimRewardsUnreceived ? "font-bold text-green-500" : "text-slate-400")}>
+                      {formatNumber(item.claimRewardsUnreceived)} FO
+                    </td>
                     <td className="text-slate-400">
-                      <a href={item.urlFull} target="_blank" rel="noreferrer" className="text-indigo-500 transition duration-150 ease-in-out hover:text-indigo-800">
+                      <a
+                        href={item.urlFull}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="text-indigo-500 transition duration-150 ease-in-out hover:text-indigo-800"
+                      >
                         {item.urlSimple}
                       </a>
                     </td>
                     <td>
-                      <Link href={"/producer/detail?account=" + item.owner} className="text-indigo-500 transition duration-150 ease-in-out hover:text-indigo-800">
+                      <Link
+                        href={"/producer/detail?account=" + item.owner}
+                        className="text-indigo-500 transition duration-150 ease-in-out hover:text-indigo-800"
+                      >
                         详情
                       </Link>
                     </td>
